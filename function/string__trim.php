0e4aee4ec3a5c52d3ce138ac3be2bf3a8156765d
Jay Yin <mumingv@163.com>
2016-03-16 10:48:23 +0800

    add function trim

---
+++ function/string__trim.php
@@ -0,0 +1,17 @@
+<?php
+/* =============================================================================
+#     FileName: string__trim.php
+#         Desc: 
+#       Author: Jie Yin
+#        Email: mumingv@163.com
+#     HomePage: https://github.com/mumingv
+#      Version: 0.0.1
+#   LastChange: 2016-03-11 20:43:35
+#      History:
+============================================================================= */
+
+function test() {
+    echo "aaa";
+}
+
+
