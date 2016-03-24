17bb8a20eb18fee83a56a58742f63cfbf3d04ba0
Jay Yin <mumingv@163.com>
2016-03-24 22:10:12 +0800

    add file string_str_replace.php

---
+++ function/string/string_str_replace.php
@@ -0,0 +1,38 @@
+<?php
+/***********************************************************
+ *     FileName: string_str_replace.php
+ *         Desc: 子字符串替换
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2016-03-24 21:59:41
+ *      History:
+ ***********************************************************/
+
+# 语法：mixed str_replace ( mixed $search , mixed $replace , mixed $subject [, int &$count ] )
+# 返回值：替换后的字符串或者数组
+
+
+# 示例
+
+// 赋值: <body text='black'>
+$bodytag = str_replace("%body%", "black", "<body text='%body%'>");
+echo $bodytag . PHP_EOL; //<body text='black'>
+
+// 赋值: Hll Wrld f PHP
+$vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
+$onlyconsonants = str_replace($vowels, "", "Hello World of PHP");
+echo $onlyconsonants . PHP_EOL; //Hll Wrld f PHP
+
+// 赋值: You should eat pizza, beer, and ice cream every day
+$phrase  = "You should eat fruits, vegetables, and fiber every day.";
+$healthy = array("fruits", "vegetables", "fiber");
+$yummy   = array("pizza", "beer", "ice cream");
+$newphrase = str_replace($healthy, $yummy, $phrase);
+echo $newphrase . PHP_EOL; //You should eat pizza, beer, and ice cream every day.
+
+// 赋值: 2
+$str = str_replace("ll", "", "good golly miss molly!", $count);
+echo $count . PHP_EOL; //2
+
