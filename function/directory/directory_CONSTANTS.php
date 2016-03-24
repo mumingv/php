bdc44f4d295c7541a3101082dad935041073b65f
Jay Yin <mumingv@163.com>
2016-03-24 19:56:39 +0800

    add file directory_CONSTANTS.php

---
+++ function/directory/directory_CONSTANTS.php
@@ -0,0 +1,19 @@
+<?php
+/***********************************************************
+ *     FileName: directory_CONSTANTS.php
+ *         Desc: 目录处理相关的预定义常量
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2016-03-24 19:53:12
+ *      History:
+ ***********************************************************/
+
+# 预定义常量
+# DIRECTORY_SEPARATOR
+# PATH_SEPARATOR
+
+echo DIRECTORY_SEPARATOR . PHP_EOL; //输出：/
+echo PATH_SEPARATOR . PHP_EOL; //输出：:
+
