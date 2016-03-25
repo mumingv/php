6b5455a3fb59df51c0dce6f135435bcf6ff85fd5
Jay Yin <mumingv@163.com>
2016-03-25 15:24:07 +0800

    add file array_usort.php

---
+++ function/array/array_usort.php
@@ -0,0 +1,38 @@
+<?php
+/***********************************************************
+ *     FileName: array_usort.php
+ *         Desc: 使用用户自定义的比较函数对数组中的值进行排序 
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2016-03-25 15:20:49
+ *      History:
+ ***********************************************************/
+
+# 语法：bool usort ( array &$array , callable $cmp_function )
+
+# 示例：
+function cmp($a, $b)
+{
+    if ($a == $b) {
+        return 0;
+    }
+    return ($a < $b) ? -1 : 1;
+}
+
+$a = array(3, 2, 5, 6, 1);
+
+usort($a, "cmp");
+
+foreach ($a as $key => $value) {
+    echo "$key: $value\n";
+}
+/*
+0: 1
+1: 2
+2: 3
+3: 5
+4: 6
+*/
+
