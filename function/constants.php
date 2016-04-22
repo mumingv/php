3ae06a927803964ecc66b6523c954f30b38e5339
Jay Yin <mumingv@163.com>
2016-04-22 12:00:51 +0800

    add file

---
+++ function/constants.php
@@ -0,0 +1,24 @@
+<?php
+/***********************************************************
+ *     FileName: constant.php
+ *         Desc: PHP的常量
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2016-04-22 11:55:37
+ *      History:
+ ***********************************************************/
+
+/**
+ * 系统常量 
+ */
+
+// PHP版本
+echo PHP_VERSION . PHP_EOL; //5.4.41 
+
+// PHP所运行的系统
+echo PHP_OS . PHP_EOL; //Linux
+
+
+
