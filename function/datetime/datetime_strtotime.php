ebf7947b6c7766dece6a237734bfb589abcde685
Jay Yin <mumingv@163.com>
2016-06-01 17:42:43 +0800

    add file

---
+++ function/datetime/datetime_strtotime.php
@@ -0,0 +1,35 @@
+<?php
+/***********************************************************
+ *     FileName: datetime_strtotime.php
+ *         Desc: 将任何英文文本的日期时间描述解析为 Unix 时间戳
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2016-06-01 15:23:33
+ *      History:
+ ***********************************************************/
+
+# 语法：int strtotime ( string $time [, int $now = time() ] )
+
+# 示例：
+//date_default_timezone_set('UTC');
+$time = time();
+echo "time(): ";
+echo($time); //1464341037
+echo PHP_EOL;
+
+$nowTime = strtotime('now');
+echo "strtotime(now): ";
+echo($nowTime);
+echo PHP_EOL;
+
+$str = '2016-06-01 07:27:13';
+echo "strtotime('2016-06-01 07:27:13'): ";
+$ret = strtotime($str);
+echo($ret);
+echo PHP_EOL;
+
+
+
+
