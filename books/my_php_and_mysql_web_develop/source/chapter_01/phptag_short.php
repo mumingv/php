58a27da3b02deeb2799292090c6eddfbd484d303
mumingv <mumingv@163.com>
2016-09-18 14:11:35 +0800

    add file

--- books/my_php_and_mysql_web_develop/source/chapter_01/phptag_short.php
+++ books/my_php_and_mysql_web_develop/source/chapter_01/phptag_short.php
@@ -6,6 +6,8 @@
         <h1>Bob's Auto Parts</h1>
         <h2>Order Results</h2>
 <?
+    // 说明：使用PHP短标记，需要打开php.ini的如下开关(此开关默认关闭)：
+    // short_open_tag = On
     echo '<p>Order processed.</p>';
 ?>
     </body>
