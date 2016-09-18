bfb005163f7b1ac31c6466d05ca01c7f73a1da45
mumingv <mumingv@163.com>
2016-09-18 14:05:03 +0800

    add file

---
+++ books/my_php_and_mysql_web_develop/source/chapter_01/phptag_short.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Bob's Auto Parts - Order Results</title>
+    </head>
+    <body>
+        <h1>Bob's Auto Parts</h1>
+        <h2>Order Results</h2>
+<?
+    echo '<p>Order processed.</p>';
+?>
+    </body>
+</html>
