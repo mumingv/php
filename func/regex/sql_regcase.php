458f272d9580f63176a4bfe869b4033e1086defb
mumingv <mumingv@163.com>
2016-10-20 16:49:05 +0800

    add file

---
+++ func/regex/sql_regcase.php
@@ -0,0 +1,17 @@
+<?php
+/***********************************************************
+ *     FileName: sql_regcase.php
+ *         Desc: 产生用于匹配的正则表达式，不区分大小写
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2016-10-20 14:07:09
+ *      History:
+ ***********************************************************/
+
+# 语法：string sql_regcase(string $string)
+
+# 示例
+echo sql_regcase("Foo - bar.");
+
