1d2231a6d8336821cf1b919b24d6e08b9c07da3c
mumingv <mumingv@163.com>
2016-10-25 11:05:45 +0800

    chapter 6

--- books/my_php_and_mysql_web_develop/chapter_06/example.php
+++ books/my_php_and_mysql_web_develop/chapter_06/example.php
@@ -42,6 +42,7 @@ $obj = new classname();
 $obj->operation('great');
  */
 
+/*
 class classname {
     protected $attribute;
     function __get($attrName) {
@@ -57,6 +58,124 @@ class classname {
 $obj = new classname();
 $obj->attribute = 'mumingv';  // call __set() function
 echo $obj->attribute;  // call _get() function
+ */
+
+// 继承
+/*
+class A {
+    public $attribute1;
+    public function operation1() {
+        echo 'In class '.__CLASS__.', $attribute1 is '.$this->attribute1.PHP_EOL; 
+    }
+}
+
+class B extends A {
+    public $attribute2;
+    public function operation2() {
+        echo 'In class '.__CLASS__.', $attribute2 is '.$this->attribute2.PHP_EOL; 
+    }
+}
+
+$b = new B();
+$b->attribute1 = 10;
+$b->operation1();
+$b->attribute2 = 20;
+$b->operation2();
+ */
+
+// 使用final关键字
+/*
+class A {
+    public $attribute = 'attr_a';
+    public function operation() {
+        echo 'In class A, $attribute is '.$this->attribute.PHP_EOL; 
+    }
+}
+
+class B extends A {
+    public $attribute = 'attr_b';
+    public function operation() {
+        echo "B's operation() is called".PHP_EOL;
+        parent::operation();
+    }
+}
+
+$b = new B();
+$b->operation();
+ */
+
+
+// 单个接口
+/*
+interface Fruit {
+    public function getSize();  // 获取颜色
+    public function getColor();  // 获取大小
+}
+
+class Apple implements Fruit {
+    public function getSize() {
+        echo 'Apple is big'.PHP_EOL;
+    }
+    public function getColor() {
+        echo 'Apple is red'.PHP_EOL;
+    }
+}
+
+class Banana implements Fruit {
+    public function getSize() {
+        echo 'Banana is small'.PHP_EOL;
+    }
+    public function getColor() {
+        echo 'Banana is yellow'.PHP_EOL;
+    }
+}
+
+$apple = new Apple();
+$apple->getSize();
+$apple->getColor();
+
+$banana = new Banana();
+$banana->getSize();
+$banana->getColor();
+ */
+
+
+// 多个接口
+class Fruit {
+    public function info() {
+        echo "I'm a fruit".PHP_EOL;
+    }
+}
+
+interface Fruit1 {
+    public function getSize();  // 获取颜色
+}
+
+interface Fruit2 {
+    public function getColor();  // 获取大小
+}
+
+
+class Apple extends Fruit implements Fruit1, Fruit2 {
+    public function getSize() {
+        echo 'Apple is big'.PHP_EOL;
+    }
+    public function getColor() {
+        echo 'Apple is red'.PHP_EOL;
+    }
+}
+
+$apple = new Apple();
+$apple->info();
+$apple->getSize();
+$apple->getColor();
+
+
+
+
+
+
+
 
 
 
