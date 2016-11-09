cd4442ca3dce66e279dd9ccbd4e713cb6635561d
mumingv <mumingv@163.com>
2016-11-09 23:25:03 +0800

    数据访问对象模式

--- books/my_php_design_patterns/chapter_05/BaseDao.php
+++ books/my_php_design_patterns/chapter_05/BaseDao.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * DAO基类 
+ * DAO基类(抽象类，不能被实例化)
+ * 需要子类提供一些信息(如：连接数据库的参数，以及表名、主键名)，才能调用该类提供的函数
  */
 abstract class BaseDao {
     private $__connection;  // 数据库连接
 
     public function __construct() {
         // 连接数据库，各参数在配置中指定
-        $this->__connectToDB(DB_USER, DB_PASS, DB_HOST, DB_DATABASE);
+        $this->__connectToDB(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
     }
 
-    private function __connectToDB($user, $pass, $host, $database) {
+    private function __connectToDB($host, $user, $pass, $database) {
         // 1. 数据库操作第一步：连接数据库
         $this->__connection = mysql_connect($host, $user, $pass);
         // 2. 数据库操作第二步：选择数据库
@@ -29,12 +30,25 @@ abstract class BaseDao {
         $sql = "SELECT * FROM {$this->_tableName} WHERE {$key} = '{$value}'";
         $results = mysql_query($sql, $this->__connection);
 
-        // 3. 数据库操作第四步：将查询结果转换为数组形式
+        // 4. 数据库操作第四步：将查询结果转换为数组形式
         $rows = array();
         while ($result = mysql_fetch_array($results)) {
             $rows[] = $result;
         }
 
         return $rows;
+    }
+
+    // 更新单条记录，$keyedArray数组中必须携带主键对应的值
+    public function update($keyedArray) {
+        $sql = "UPDATE {$this->_tableName} SET ";
+        $updates = array();
+        foreach ($keyedArray as $column => $value) {
+            $updates[] = "{$column} = '{$value}'";
+        }
+        $sql .= implode(', ', $updates);
+        $sql .= " WHERE {$this->_primaryKey} = '{$keyedArray[$this->_primaryKey]}'";
+        // 5. 数据库操作：更新数据库(使用的函数'mysql_query'和查询数据库的操作是一样的)
+        mysql_query($sql, $this->__connection);
     }
 }
--- books/my_php_design_patterns/chapter_05/index.php
+++ books/my_php_design_patterns/chapter_05/index.php
@@ -7,13 +7,28 @@ require_once('BaseDao.php');
 require_once('User.php');
 
 // 下面这些配置需要放在配置文件中
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'Mysql2016!');
-define('DB_HOST', 'localhost');
 define('DB_DATABASE', 'pdp');
 
 // 主程序
-$user = new User();
-$result = $user->query(1);
-var_dump($result);
+
+// 使用基类提供的方法，查询主键(id)值为1的用户信息
+$userObj = new User();
+$userInfo = $userObj->query(1);
+echo "User info number: ".count($userInfo).PHP_EOL;
+foreach ($userInfo as $user) {
+    echo "id: ".$user['id'].", name: ".$user['name'].", tel: ".$user['tel'].PHP_EOL;
+}
+
+// 使用基类提供的方法，更新主键(id)值为1的用户信息
+$userObj->update(array('id' => 1, 'name' => 'Jay', 'tel' => '18612345678'));
+
+// 使用派生类提供的方法，查询名字为'Jay'的所有用户信息
+$userInfo = $userObj->getUserByName('Jay');
+echo "User info number: ".count($userInfo).PHP_EOL;
+foreach ($userInfo as $user) {
+    echo "id: ".$user['id'].", name: ".$user['name'].", tel: ".$user['tel'].PHP_EOL;
+}
 
