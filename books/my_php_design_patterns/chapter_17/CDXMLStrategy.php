6c31925f2cfbbe66c2b363cf60590f0b0594a44e
mumingv <mumingv@163.com>
2016-11-15 18:09:32 +0800

    策略模式

--- books/my_php_design_patterns/chapter_17/CDXMLStrategy.php
+++ books/my_php_design_patterns/chapter_17/CDXMLStrategy.php
@@ -6,7 +6,6 @@
 class CDXMLStrategy {
     // 该函数在CD类中被调用，参数为CD类对象是因为该函数中要获取CD类对象的属性
     public function get(CD $cd) {
-        /*
         $doc = new DomDocument();
 
         $root = $doc->createElement('CD');
@@ -18,8 +17,7 @@ class CDXMLStrategy {
         $band = $doc->createElement('BAND', $cd->band);
         $band = $root->appendChild($band);
 
+        echo $doc->saveXML();
         return $doc->saveXML();
-         */
-        echo __CLASS__.PHP_EOL;
     }
 }
