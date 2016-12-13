693c4084b333922a717af972eef4d21a7eed9977
mumingv <mumingv@163.com>
2016-12-13 19:48:23 +0800

    序列化和反序列化

--- demo/string/demo_array_to_json/demo_array_to_json.php
+++ demo/string/demo_array_to_json/demo_array_to_json.php
@@ -1,7 +1,16 @@
 <?php
-
+/***********************************************************
+ *     FileName: demo_array_to_json.php
+ *         Desc: 从文件读取数组打印字符串，并将其转换为数组变量
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2016-12-13 19:42:47
+ *      History:
+ ***********************************************************/
 
 $arr_str = file_get_contents('input');
-$arr = $arr_str;
-$arr['foo'] = 'bar';
-print_r($arr);
+eval('$arr ='.$arr_str.';');
+//$arr['foo'] = 'bar';  // 测试：向数组中插入一个字段
+file_put_contents('output', json_encode($arr));
