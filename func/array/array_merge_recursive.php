c6d08ad50f9f14ba0d4e89979e8749f3846ca18a
mumingv <mumingv@163.com>
2017-01-05 12:14:26 +0800

    add

---
+++ func/array/array_merge_recursive.php
@@ -0,0 +1,39 @@
+<?php
+/***********************************************************
+ *     FileName: array_merge_recursive.php
+ *         Desc: 递归地合并一个或多个数组
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2017-01-05 12:02:15
+ *      History:
+ ***********************************************************/
+
+# 语法：array array_merge_recursive ( array $array1 [, array $... ] )
+
+# 示例：
+$ar1 = array("color" => array("favorite" => "red"), 5);
+$ar2 = array(10, "color" => array("favorite" => "green", "blue"));
+$result = array_merge_recursive($ar1, $ar2);
+print_r($result);
+/*
+Array
+(
+    [color] => Array
+        (
+            [favorite] => Array
+                (
+                    [0] => red
+                    [1] => green
+                )
+
+            [0] => blue
+        )
+
+    [0] => 5
+    [1] => 10
+)
+*/
+
+
