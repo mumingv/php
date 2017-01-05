a8f61e43f4ffa54594470063cb1d0aa81588a38c
mumingv <mumingv@163.com>
2017-01-05 21:37:03 +0800

    add

--- func/json/json_decode.php
+++ func/json/json_decode.php
@@ -28,3 +28,113 @@ $output = json_decode($json_str, true);
 print_r("output is: \n");
 print_r($output);
 
+
+# 示例：常用用法，将JSON字符串转换成一个PHP数组
+$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+print_r(json_decode($json, true));
+/*
+Array
+(
+    [a] => 1
+    [b] => 2
+    [c] => 3
+    [d] => 4
+    [e] => 5
+)
+*/
+
+
+# 示例：非常用用法，将JSON字符串转换成一个PHP对象
+$json = '{"a":1,"b":2,"c":3,"d":4,"e":5,"foo-bar":6}';
+$obj = json_decode($json);
+print_r($obj);
+print_r($obj->c);  // 访问对象的元素
+echo "\n";
+print_r($obj->{'foo-bar'});  // 访问对象的元素
+echo "\n";
+/*
+stdClass Object
+(
+    [a] => 1
+    [b] => 2
+    [c] => 3
+    [d] => 4
+    [e] => 5
+)
+3
+6
+*/
+
+
+# 超大整数的处理
+$json = '{"number": 12345678901234567890}';
+var_dump(json_decode($json));
+var_dump(json_decode($json, false, 512, JSON_BIGINT_AS_STRING));
+/*
+object(stdClass)#2 (1) {
+  ["number"]=>
+  float(1.2345678901235E+19)
+}
+object(stdClass)#2 (1) {
+  ["number"]=>
+  string(20) "12345678901234567890"
+}
+*/
+
+
+# 深度错误
+$json = json_encode(
+    array(
+        1 => array(
+            'English' => array(
+                'One',
+                'January'
+            ),
+            'French' => array(
+                'Une',
+                'Janvier'
+            )
+        )
+    )
+);
+// 获取JSON相关的错误常量数组(key为常量值，value为常量名称)
+$constants = get_defined_constants(true);
+$json_errors = array();
+foreach ($constants["json"] as $name => $value) {
+    if (!strncmp($name, "JSON_ERROR_", 11)) {
+        $json_errors[$value] = $name;
+    }
+}
+foreach (range(4, 3, -1) as $depth) {
+    var_dump(json_decode($json, true, $depth));
+    echo 'Last error: ', $json_errors[json_last_error()], PHP_EOL, PHP_EOL;
+}
+/*
+array(1) {
+  [1]=>
+  array(2) {
+    ["English"]=>
+    array(2) {
+      [0]=>
+      string(3) "One"
+      [1]=>
+      string(7) "January"
+    }
+    ["French"]=>
+    array(2) {
+      [0]=>
+      string(3) "Une"
+      [1]=>
+      string(7) "Janvier"
+    }
+  }
+}
+Last error: JSON_ERROR_NONE
+
+NULL
+Last error: JSON_ERROR_DEPTH
+*/
+
+
+
+
