b4301478007fea4e23aaac86cab8b07cd2fc26fc
mumingv <mumingv@163.com>
2017-01-05 14:06:50 +0800

    add

--- func/array/array_rand.php
+++ func/array/array_rand.php
@@ -6,18 +6,27 @@
  *        Email: mumingv@163.com
  *     HomePage: https://github.com/mumingv
  *      Version: 0.0.1
- *   LastChange: 2016-06-29 16:36:38
+ *   LastChange: 2017-01-05 13:58:06
  *      History:
  ***********************************************************/
 
-# 语法：number array_sum ( array $array )
+# 语法：mixed array_rand ( array $input [, int $num_req = 1 ] )
 
-# 示例：
+# 示例：随机获取一个元素的键名
 $input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
-$rand_keys = array_rand($input, 2);
-echo $input[$rand_keys[0]] . "\n";
-echo $input[$rand_keys[1]] . "\n";
-
 $rand_key = array_rand($input);
-print_r($rand_key);
+print_r($rand_key);  // 4
+echo "\n";
+
+
+# 示例：随机获取两个元素的键名
+$rand_keys = array_rand($input, 2);
+print_r($rand_keys);
+/*
+Array
+(
+    [0] => 0
+    [1] => 2
+)
+*/
 
