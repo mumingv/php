c0342299d06d7a4fcca441a31d87e09606bd6186
mumingv <mumingv@163.com>
2017-01-05 15:35:36 +0800

    add

--- func/array/array_reverse.php
+++ func/array/array_reverse.php
@@ -1,12 +1,12 @@
-G<?php
+<?php
 /***********************************************************
- *     FileName: array_array_reverse.php
+ *     FileName: array_reverse.php
  *         Desc: 返回一个单元顺序相反的数组 
  *       Author: Jie Yin
  *        Email: mumingv@163.com
  *     HomePage: https://github.com/mumingv
  *      Version: 0.0.1
- *   LastChange: 2016-04-19 16:55:07
+ *   LastChange: 2017-01-05 15:27:49
  *      History:
  ***********************************************************/
 
