49b1d144cd9e23ca181fffd487cf1f51803ea953
mumingv <mumingv@163.com>
2017-01-12 15:21:09 +0800

    add

--- func/strings/bin2hex.php
+++ func/strings/bin2hex.php
@@ -1,7 +1,7 @@
 <?php
 /***********************************************************
  *     FileName: string_bin2hex.php
- *         Desc: 函数把ASCII字符的字符串转换为十六进制值
+ *         Desc: 函数把包含数据的二进制字符串转换为十六进制值
  *       Author: Jie Yin
  *        Email: mumingv@163.com
  *     HomePage: https://github.com/mumingv
@@ -10,12 +10,17 @@
  *      History:
  ***********************************************************/
 
+# 说明：该函数将字符串中的每个字符逐个转换成对应的ASCII码16进制表示。
+
 # 语法：string bin2hex ( string $str )
 
 # 示例
 $binary = "11111001";
 $hex = bin2hex($binary);
 echo $hex, PHP_EOL; //3131313131303031
+
+echo bin2hex('abc'), PHP_EOL;  // 616263
+
 $hex = dechex(bindec($binary));
 echo $hex, PHP_EOL; //f9
 
