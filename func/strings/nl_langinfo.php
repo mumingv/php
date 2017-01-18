917442fdd1fa10c936e5252431d18faef4b06e5b
mumingv <mumingv@163.com>
2017-01-19 00:02:22 +0800

    add

---
+++ func/strings/nl_langinfo.php
@@ -0,0 +1,15 @@
+<?php
+/***********************************************************
+ *     FileName: nl_langinfo.php
+ *         Desc: Query language and locale information
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2017-01-19 00:01:11
+ *      History:
+ ***********************************************************/
+
+# 语法：string nl_langinfo ( int $item )
+
+# 示例：一般不使用，略。
