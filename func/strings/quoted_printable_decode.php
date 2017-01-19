d2f152f4fe1aa1e6018c11f3177e08cf04b5d5df
mumingv <mumingv@163.com>
2017-01-20 00:40:07 +0800

    add

---
+++ func/strings/quoted_printable_decode.php
@@ -0,0 +1,15 @@
+<?php
+/***********************************************************
+ *     FileName: quoted_printable_decode.php
+ *         Desc: 将 quoted-printable 字符串转换为 8-bit 字符串
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2017-01-20 00:38:06
+ *      History:
+ ***********************************************************/
+
+# 语法：string quoted_printable_decode ( string $str )
+
+# 示例：不使用该函数，略。
---
+++ func/strings/quoted_printable_encode.php
@@ -0,0 +1,15 @@
+<?php
+/***********************************************************
+ *     FileName: quoted_printable_encode.php
+ *         Desc: 将 8-bit 字符串转换成 quoted-printable 字符串
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2017-01-20 00:39:13
+ *      History:
+ ***********************************************************/
+
+# 语法：string quoted_printable_encode ( string $str )
+
+# 示例：不使用该函数，略。
