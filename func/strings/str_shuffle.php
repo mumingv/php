11ecf2f89fd584f7dd189c6bb8737855b64b2849
mumingv <mumingv@163.com>
2017-02-07 15:24:05 +0800

    add

---
+++ func/strings/str_shuffle.php
@@ -0,0 +1,17 @@
+<?php
+/***********************************************************
+ *     FileName: str_shuffle.php
+ *         Desc: 随机打乱一个字符串
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2017-02-07 15:20:47
+ *      History:
+ ***********************************************************/
+
+# 语法：string str_shuffle ( string $str )
+
+# 示例：
+echo str_shuffle('abcdef');  // aecbdf
+
---
+++ func/strings/str_split.php
@@ -0,0 +1,45 @@
+<?php
+/***********************************************************
+ *     FileName: str_split.php
+ *         Desc: 将字符串转换为数组
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2017-02-07 15:22:54
+ *      History:
+ ***********************************************************/
+
+# 语法：array str_split ( string $string [, int $split_length = 1 ] )
+
+# 示例：
+$str = "Hello Friend";
+$arr1 = str_split($str);
+$arr2 = str_split($str, 3);
+print_r($arr1);
+print_r($arr2);
+/*
+Array
+(
+    [0] => H
+    [1] => e
+    [2] => l
+    [3] => l
+    [4] => o
+    [5] =>  
+    [6] => F
+    [7] => r
+    [8] => i
+    [9] => e
+    [10] => n
+    [11] => d
+)
+Array
+(
+    [0] => Hel
+    [1] => lo 
+    [2] => Fri
+    [3] => end
+)
+*/
+
