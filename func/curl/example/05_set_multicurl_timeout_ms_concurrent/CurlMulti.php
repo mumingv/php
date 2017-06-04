047cc60028e71cd537713439e4e244c183aec1db
mumingv <mumingv@163.com>
2017-06-04 21:11:58 +0800

    add

--- func/curl/example/05_set_multicurl_timeout_ms_concurrent/CurlMulti.php
+++ func/curl/example/05_set_multicurl_timeout_ms_concurrent/CurlMulti.php
@@ -1,7 +1,7 @@
 <?php
 /***********************************************************
  *     FileName: CurlMulti.php
- *         Desc: 并发Curl，支持毫秒级超时
+ *         Desc: 并发Curl，支持毫秒级超时(一个url并发多)
  *       Author: Jie Yin
  *        Email: mumingv@163.com
  *     HomePage: https://github.com/mumingv
@@ -15,7 +15,7 @@
  */
 class CurlMulti {
     public function execute($urls, $options = array()) {  
-        $ch= array();  // 存放句柄，二维数组，与urls对应
+        $chArr= array();  // 存放句柄，二维数组，与urls对应
         $results = array();  // 存放结果，二维数组，第一维与urls的第一维对应 
         $mh = curl_multi_init();  
 
@@ -29,20 +29,20 @@ class CurlMulti {
         
         foreach ($urls as $key => $urlArr) {  
             foreach ($urlArr as $inkey => $url) {
-                $ch[$key][$inkey] = curl_init();
+                $chArr[$key][$inkey] = curl_init();
                 if ($options) {  
-                    curl_setopt_array($ch[$key][$inkey], $options);  
+                    curl_setopt_array($chArr[$key][$inkey], $options);  
                 }  
-                curl_setopt($ch[$key][$inkey], CURLOPT_URL, $url);  
-                curl_multi_add_handle($mh, $ch[$key][$inkey]);
+                curl_setopt($chArr[$key][$inkey], CURLOPT_URL, $url);  
+                curl_multi_add_handle($mh, $chArr[$key][$inkey]);
                 $results[$key][$inkey] = array(
                     'url' => $url,
                     'errmsg' => '',
                     'content' => '',
                 );
             }
-        }  
-        //print_r($ch);print_r($results);print_r($mh);exit;
+        }
+        //print_r($chArr);print_r($results);print_r($mh);exit;
         
         $running = null;  
         do {  
@@ -50,7 +50,7 @@ class CurlMulti {
         } while ($running > 0);  
         
         // 获取内容并移除句柄
-        foreach ($ch as $key => $handleArr) {  
+        foreach ($chArr as $key => $handleArr) {  
             foreach ($handleArr as $inkey => $handle)
             {
                 $content = curl_multi_getcontent($handle);  
