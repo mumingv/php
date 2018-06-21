dcb637a062721b91599986e6b8f7ef33342efb03
mumingv <mumingv@163.com>
2018-06-21 09:52:33 +0800

    3.请求和响应->请求信息

--- books/thinkphp5_quickstart/tp5/application/index/controller/IndexRequest.php
+++ books/thinkphp5_quickstart/tp5/application/index/controller/IndexRequest.php
@@ -12,4 +12,16 @@ class IndexRequest
         echo 'url: ' . $request->url() . '<br/>';
         return 'Hello,' . $name . '！';
     }
+
+	// 3.请求和响应->请求信息
+    public function hello2(Request $request)
+    {
+        echo '请求参数：';
+        dump($request->param());
+        echo 'name:'.$request->param('name');
+
+		// 助手函数
+		dump(input());
+		echo 'name:'.input('name');	
+    }
 }
