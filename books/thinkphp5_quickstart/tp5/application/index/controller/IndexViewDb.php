4e67dd675f2a9208531c2b2d5565f73cb5b34d62
mumingv <mumingv@163.com>
2018-07-18 01:35:05 +0800

    4.数据库->原生查询

--- books/thinkphp5_quickstart/tp5/application/index/controller/IndexViewDb.php
+++ books/thinkphp5_quickstart/tp5/application/index/controller/IndexViewDb.php
@@ -12,4 +12,44 @@ class IndexViewDb extends Controller
         $this->assign('result', $data); 
         return $this->fetch();
     }
+
+    /**
+     * 创建表记录
+     * @access public
+     * @return void
+     */
+    public function create() {
+        $result = Db::execute('insert into think_data_2 (id, name ,status) values (5, "thinkphp",1)');
+        dump($result);
+    }
+
+    /**
+     * 更新表记录
+     * @access public
+     * @return void
+     */
+    public function update() {
+        $result = Db::execute('update think_data_2 set name = "framework" where id = 5 ');
+        dump($result);
+    }
+
+    /**
+     * 读取表记录
+     * @access public
+     * @return void
+     */
+    public function read() {
+        $result = Db::query('select * from think_data_2 where id = 5');
+        dump($result);
+    }
+
+    /**
+     * 删除表记录
+     * @access public
+     * @return void
+     */
+    public function delete() {
+        $result = Db::execute('delete from think_data_2 where id = 5 ');
+        dump($result);
+    }
 }
